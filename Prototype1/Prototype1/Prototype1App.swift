ad96ab03b261be52025ef20819a8c9c90f7d2890
G4ndhiV <145467531+G4ndhiV@users.noreply.github.com>
2024-08-30 13:01:49 -0600

    Add files via upload

---
+++ Prototype1/Prototype1/Prototype1App.swift
@@ -0,0 +1,23 @@
+//
+//  Prototype1App.swift
+//  Prototype1
+//
+//  Created by Josue Galindo on 29/08/24.
+//
+
+import SwiftUI
+
+@main
+struct Prototype1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
+
+struct Previews_Prototype1App_Previews: PreviewProvider {
+    static var previews: some View {
+        /*@START_MENU_TOKEN@*/Text("Hello, World!")/*@END_MENU_TOKEN@*/
+    }
+}
